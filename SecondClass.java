b981b151e66c03df17325c5ba361312e48cd2c0a
AkobirTuraev <AkobirfTuraev@gmail.com>
2019-12-12 10:41:57 -0500

    first

---
+++ SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String myname = "Luis patron";
+        System.out.println(myname);
+        System.out.println("this  is another line");
+        System.out.println("this  is another line");
+        System.out.println("this  is another line");
+    }
+}
